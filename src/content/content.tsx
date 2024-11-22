c9430e704522a7ac2430fc73f5be0e01b3ab93a7
Sahib.Aujla <sahibpreet568@gmail.com>
2024-11-22 13:51:28 -0500

    updated the url

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -2,9 +2,11 @@ import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const ContentPage: React.FC = () => {
-  return <div className='z-50 fixed bottom-8 right-8'>
-    <Button variant={'destructive'}>Btn here</Button>
-  </div>
+  return (
+    <div className="z-50 fixed bottom-8 right-8">
+      <Button  className="w-[100px] h-[80px]">Ask AI</Button>
+    </div>
+  )
 }
 
 export default ContentPage
