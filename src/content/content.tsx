b4d4d7133408d3a643de4be10f0a08eb494a1a25
Sahib.Aujla <sahibpreet568@gmail.com>
2024-11-25 09:04:14 -0500

    added chatbox

--- src/content/content.tsx
+++ src/content/content.tsx
@@ -1,10 +1,21 @@
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import ChatBox from '@/components/ui/ChatBox'
+import React, { useState } from 'react'
 
 const ContentPage: React.FC = () => {
+  const [isExpanded, setIsExpanded] = useState(false)
   return (
     <div className="z-50 fixed bottom-8 right-8">
-      <Button  className="w-[100px] h-[80px]">Ask AI</Button>
+      {!isExpanded ? (
+        <Button
+          onClick={() => setIsExpanded(true)}
+          className="w-[100px] h-[80px]"
+        >
+          Ask AI
+        </Button>
+      ) : (
+        <><ChatBox/></>
+      )}
     </div>
   )
 }
