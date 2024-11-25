13d246bc56444dc3e8b843c01b0f83e3d04d9de1
Sahib.Aujla <sahibpreet568@gmail.com>
2024-11-25 09:23:20 -0500

    added chatbox popup

--- src/components/ui/ChatBox.tsx
+++ src/components/ui/ChatBox.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const ChatBox = () => {
   return (
-    <div>ChatBox</div>
+    <div className='z-[500] w-[500px] h-[600px] fixed bottom-8 right-8 bg-red-500'>ChatBox</div>
   )
 }
 
--- src/content/content.tsx
+++ src/content/content.tsx
@@ -5,18 +5,20 @@ import React, { useState } from 'react'
 const ContentPage: React.FC = () => {
   const [isExpanded, setIsExpanded] = useState(false)
   return (
-    <div className="z-50 fixed bottom-8 right-8">
+    <>
       {!isExpanded ? (
-        <Button
-          onClick={() => setIsExpanded(true)}
-          className="w-[100px] h-[80px]"
-        >
-          Ask AI
-        </Button>
+        <div className="z-50 fixed bottom-8 right-8">
+          <Button
+            onClick={() => setIsExpanded(true)}
+            className="w-[100px] h-[80px]"
+          >
+            Ask AI
+          </Button>
+        </div>
       ) : (
-        <><ChatBox/></>
+        <ChatBox />
       )}
-    </div>
+    </>
   )
 }
 
