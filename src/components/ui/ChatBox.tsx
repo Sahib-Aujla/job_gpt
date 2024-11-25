13c917f7ff335fadd4e608178d679c6207804214
Sahib.Aujla <sahibpreet568@gmail.com>
2024-11-25 09:25:04 -0500

    added input

--- src/components/ui/ChatBox.tsx
+++ src/components/ui/ChatBox.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Input } from './input'
 
 const ChatBox = () => {
   return (
-    <div className='z-[500] w-[500px] h-[600px] fixed bottom-8 right-8 bg-red-500'>ChatBox</div>
+    <div className="z-[500] w-[500px] h-[600px] fixed bottom-8 right-8">
+      <div></div>
+      <div>
+        <Input
+          placeholder="Enter Open API key"
+          type="text"
+          className="flex-grow text-sm px-4 py-2 rounded-md bg-gray-700 text-gray-200 border border-gray-600 focus:ring-2 focus:ring-blue-500 outline-none"
+        />
+      </div>
+    </div>
   )
 }
 
